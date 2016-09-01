5ca605f1471e8142ba7e97632d190ccbe79def2a
jiongdu <dujiong.uestc@gmail.com>
2016-09-01 19:01:45 +0800

    2016-9-1

---
+++ 校招编程题/华为/字符集合/字符集合.cpp
@@ -0,0 +1,31 @@
+/**************************************
+* @author dujiong
+* @date 2016.9.1
+* @version V0.1
+**************************************/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void stringSet(string s){
+    int temp[259]={0};
+    for(int i=0;i<s.size();i++){
+        temp[s[i]]++;
+        if(temp[s[i]]==1){
+            cout << s[i];
+        }
+    }
+    cout << endl;
+}
+
+int main()
+{
+    string a;
+    while(cin >> a){
+        stringSet(a);
+    }
+
+    return 0;
+}
