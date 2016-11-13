95b288320626045e3a443c8f70556821bd6d0760
jiongdu <dujiong.uestc@gmail.com>
2016-11-13 20:43:20 +0800

    2016-11-13

---
+++ leetcode/climbing-stairs/README.md
@@ -0,0 +1,9 @@
+﻿题目描述:
+You are climbing a stair case. It takes n steps to reach to the top.
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+题目解析:
+最简单的动态规划，转移方程：dp[n]=dp[n-1]+dp[n-2]
+
+题目地址:
+http://www.nowcoder.com/practice/d679cfa563974385a3bef8cd854c73db?tpId=46&tqId=29108&rp=4&ru=/ta/leetcode&qru=/ta/leetcode/question-ranking
---
+++ leetcode/climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,19 @@
+/**************************************
+* @author dujiong
+* @date 2016.11.13
+* @version V0.1
+**************************************/
+
+class Solution {
+public:
+    int climbStairs(int n) {
+    	int dp[n+1];
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
+       	for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+};
