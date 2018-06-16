2359a38653d033bd217720382de2cf50454b72c4
jiongdu <jonnydu.uestc@gmail.com>
2018-06-16 14:35:07 +0800

    add golang implementation

---
+++ leetcode-golang/Two Sum/Two Sum.go
@@ -0,0 +1,10 @@
+func twoSum(nums []int, target int) []int {
+	lookup := make(map[int]int)
+	for i, num := range nums {
+		if j, ok := lookup[target-num]; ok {
+			return []int{i, j}
+		}
+		lookup[nums[i]] = i
+	}
+	return nil
+}
