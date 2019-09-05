ce348e092e2a68cf4a9eb09bb1f999b5e17c8825
jiongdu <jonnydu.uestc@gmail.com>
2019-09-05 10:05:56 +0800

    merge intervals

--- leetcode-golang/Merge Intervals/Merge Intervals.go
+++ leetcode-golang/Merge Intervals/Merge Intervals.go
@@ -15,17 +15,34 @@ func merge(intervals [][]int) [][]int {
 
 	//如果第二个case的判定和第三个之间顺序错误，会导致不满足intervals[i][0] < current[1]的情况也会被误判定为
 	//intervals[i][1] > current[1]，导致没有集合交集的也进行了合并
+	//原代码中没有使用break的原因是各case条件之间不存在冲突，所以不会出现顺序执行的情况，各case结束时加break效果一样
+
+	//这样可能会好理解些
 	for i := range intervals {
 		switch {
 		case current == nil:
 			current = intervals[i]
-		case intervals[i][0] > current[1]:
+			break
+		case current[1] < intervals[i][0]:
 			merged = append(merged, current)
 			current = intervals[i]
-		case intervals[i][1] > current[1]:
+			break
+		case current[1] < intervals[i][1]: //暗含current[1] > intervals[i][0]
 			current[1] = intervals[i][1]
+			break
 		}
 	}
+	// for i := range intervals {
+	// 	switch {
+	// 	case current == nil:
+	// 		current = intervals[i]
+	// 	case intervals[i][0] > current[1]:
+	// 		merged = append(merged, current)
+	// 		current = intervals[i]
+	// 	case intervals[i][1] > current[1]:
+	// 		current[1] = intervals[i][1]
+	// 	}
+	// }
 	if current != nil {
 		merged = append(merged, current)
 	}
