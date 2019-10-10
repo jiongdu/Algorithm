9e892cc2ef4d35e43bcf38de52391bfa99249e96
jiongdu <jonnydu.uestc@gmail.com>
2019-10-11 00:07:14 +0800

    leetcode-golang

--- leetcode-golang/Decode Ways/Decode Ways.go
+++ leetcode-golang/Decode Ways/Decode Ways.go
@@ -9,7 +9,7 @@ func numDecodings(s string) int {
 	lastOne, lastTwo := int(s[0]-'0'), 0
 	for i := 2; i <= length; i++ {
 		last := int(s[i-1] - '0')
-		lastOne, lastTwo = last, lastOne*10+last
+		lastOne, lastTwo = last, lastOne*10+last //所以这里也不要使用Atoi(s[i-1:i+1])的方式，而是直接取每一位来计算，可以避免05转换成5这种问题
 		if lastOne >= 1 {
 			dp[i] = dp[i-1]
 		}
