4ccc9c656f2a3ab65fd5ebab18efbb0b8b820eb2
jiongdu <jonnydu.uestc@gmail.com>
2020-03-03 14:03:16 +0800

    strings.builder

--- leetcode-golang/Reverse Words in a String III/Reverse Words in a String III.go
+++ leetcode-golang/Reverse Words in a String III/Reverse Words in a String III.go
@@ -1,3 +1,8 @@
+import (
+	"fmt"
+	"strings"
+)
+
 func reverseWords(s string) string {
 	//不能用string，因为string是不可变的，所以不能赋值
 	bs := []byte(s)
@@ -41,4 +46,25 @@ func reverse(src []byte, from int, to int) {
 		from++
 		to--
 	}
+}
+
+//使用string.Builder可以提升性能
+func reverseWords(s string) string {
+	if len(s) == 0 {
+		return ""
+	}
+	strSlice := strings.Fields(s)
+	if len(strSlice) == 0 {
+		return ""
+	}
+	for i := 0; i < len(strSlice)/2; i++ {
+		strSlice[i], strSlice[len(strSlice)-1-i] = strSlice[len(strSlice)-1-i], strSlice[i]
+	}
+	var b strings.Builder
+	for i := 0; i < len(strSlice)-1; i++ {
+		fmt.Fprint(&b, strSlice[i])
+		fmt.Fprint(&b, " ")
+	}
+	fmt.Fprint(&b, strSlice[len(strSlice)-1])
+	return b.String()
 }
